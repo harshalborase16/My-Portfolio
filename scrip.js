5fed7bde35d6e3c49f65a1f4fc71a71c905f085b
Harshal patil <harshalborase2005@gmail.com>
2026-01-26 14:39:21 +0530

    hj

--- scrip.js
+++ scrip.js
@@ -6,3 +6,35 @@
     navMenu.classList.toggle('active');
     hamburger.classList.toggle('active');
   });
+
+
+  // contact form              
+
+
+
+(function () {
+    emailjs.init("-G1rwwiAkLZPhlAwz");
+})();
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_fo96nwh",  
+      "template_99u9hg4",  
+      this
+    ).then(
+      function () {
+        document.getElementById("status").innerHTML =
+          "Message sent successfully ✅";
+        document.getElementById("contact-form").reset();
+      },
+      function (error) {
+        document.getElementById("status").innerHTML =
+          "Failed to send ❌";
+        console.log(error);
+      }
+    );
+  });
